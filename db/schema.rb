eba206771f5e2ef2a418fc4afd644f883587478f
“kylemharman” <kylemartinharman@gmail.com>
2019-11-28 21:13:26 +1100

    added gems and connected to react

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_11_28_084005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "planes", force: :cascade do |t|
+    t.text "name"
+    t.integer "rows"
+    t.integer "columns"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
